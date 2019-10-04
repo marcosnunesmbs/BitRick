5d6291ead6d02c7325cf9d7f4aeb07642117092c
Marcos Nunes <marcosnunesmbs@gmail.com>
2019-10-04 01:02:12 -0300

    change telegram bot config

--- src/config/telegram.js
+++ src/config/telegram.js
@@ -1,12 +1,10 @@
-// const TelegramBot = require('node-telegram-bot-api')
+var config = require("./config.json")
+
 const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
 
-
-var config = require("./config.json")
 const token = config.botToken
-// const bot = new TelegramBot(token, {polling: true})
 
 const bot = new Telegraf(token)
 
