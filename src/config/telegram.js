9b53e70006ea454fedccb5a0ae6479673ea0d912
Marcos Nunes <marcosnunesmbs@gmail.com>
2019-10-04 00:59:40 -0300

    last changes

--- src/config/telegram.js
+++ src/config/telegram.js
@@ -1,6 +1,17 @@
-const TelegramBot = require('node-telegram-bot-api')
+// const TelegramBot = require('node-telegram-bot-api')
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+
+
 var config = require("./config.json")
 const token = config.botToken
-const bot = new TelegramBot(token, {polling: true})
+// const bot = new TelegramBot(token, {polling: true})
 
-module.exports = bot
+const bot = new Telegraf(token)
+
+module.exports = {
+    bot,
+    Extra,
+    Markup
+}
--- src/plugins/binance/index.js
+++ src/plugins/binance/index.js
@@ -1,71 +1,71 @@
 const Exchange = require('./services/binance')
-const bot = require('../../config/telegram')
+const { bot } = require('../../config/telegram')
+const { Extra } = require('../../config/telegram')
+const { Markup } = require('../../config/telegram')
+
 var responses = require('./reponses')
 var config = require('./config.json')
-   
-bot.on('message', (msg, match) => {
-    var chatID = msg.chat.id
-    var userName =  msg.from.first_name
-    var balance = "bbalance"
-    var trade = "bprice"
-    var orders = "borders"
 
-    if (msg.text.toString().toLowerCase().indexOf(balance) == 0) {
-        var currency = msg.text.split(" ")
-        if (!currency[1]) {
-            bot.sendMessage(msg.chat.id, responses.get('balanceError', userName))
-        } else {
-            Exchange.getBalances(currency[1], msg.from.username)
-            .then(resp => bot.sendMessage(msg.chat.id, resp))
-        }
-    }
-    if (msg.text.toString().toLowerCase().indexOf(trade) == 0) {
-        var currency = msg.text.split(" ")
-        if (!currency[1]) {
-            bot.sendMessage(msg.chat.id, responses.get('tradeError', userName))
-        } else {
-            Exchange.getPrice(currency[1], msg.from.username)
-            .then(resp => bot.sendMessage(msg.chat.id, resp))
-        }
-    }
-    if (msg.text.toString().toLowerCase().indexOf(orders) == 0) {
-        var currency = msg.text.split(" ")
-        if (!currency[1]) {
-            bot.sendMessage(msg.chat.id, responses.get('ordersError', userName))
-        } else {
-            Exchange.getOrders(currency[1], msg.from.username)
-            .then(resp => bot.sendMessage(msg.chat.id, resp))
-        }
-    }
-})
+const balanceButtons = Markup.inlineKeyboard([
+    Markup.callbackButton('BTC', 'getBalance btc'),
+    Markup.callbackButton('DOGE', 'getBalance doge'),
+    Markup.callbackButton('DOGE', 'getBalance doge'),
+    Markup.callbackButton('BNB', 'getBalance bnb'),
+    Markup.callbackButton('LINK', 'getBalance link'),
+    Markup.callbackButton('DGB', 'getBalance dgb'),
+    Markup.callbackButton('NANO', 'getBalance nano'),
+    Markup.callbackButton('USDT', 'getBalance usdt'),
+  ], { columns: 3 }).extra()
 
-    bot.onText(/\/bbalance/, (msg, match) => {
-        var currency = match.input.split(" ")
+  const priceButtons = Markup.inlineKeyboard([
+    Markup.callbackButton('DASH', 'getPrice dash'),
+    Markup.callbackButton('DOGE', 'getPrice doge'),
+    Markup.callbackButton('DOGE', 'getPrice doge'),
+    Markup.callbackButton('BNB', 'getPrice bnb'),
+    Markup.callbackButton('LINK', 'getPrice link'),
+    Markup.callbackButton('DGB', 'getPrice dgb'),
+    Markup.callbackButton('NANO', 'getPrice nano'),
+    Markup.callbackButton('BTC', 'getPrice btc'),
+  ], { columns: 3 }).extra()
+
+    bot.command('bbalance', (ctx) => {
+        var currency = ctx.message.text.split(" ")
         if (!currency[1]) {
-            bot.sendMessage(msg.chat.id, "Por favor, digite /bbalance + MOEDA para saber o saldo. Ex:\n /saldo BTC")
+            ctx.reply('Escolha uma moeda para saber o seu Saldo', balanceButtons)
         } else {
-            Exchange.getBalances(currency[1], msg.from.username)
-            .then(resp => bot.sendMessage(msg.chat.id, resp))
+            Exchange.getBalances(currency[1], ctx.from.first_name)
+            .then(resp => ctx.reply(resp))
         }
     })
 
-    bot.onText(/\/bprice/, (msg, match) => {
-        var currency = match.input.split(" ")
+    bot.command('bprice', (ctx) => {
+        var currency = ctx.message.text.split(" ")
         if (!currency[1]) {
-            bot.sendMessage(msg.chat.id, "Por favor, digite /bprice + SÍMBOLO para saber o saldo. Ex:\n /cotação BTCBNB")
+            ctx.reply('Escolha uma moeda para saber a Cotação', priceButtons)
         } else {
-            Exchange.getPrice(currency[1], msg.from.username)
-            .then(resp => bot.sendMessage(msg.chat.id, resp))
+            Exchange.getPrice(currency[1], ctx.from.first_name)
+            .then(resp => ctx.reply(resp))
         } 
     })
-    bot.onText(/\/borders/, (msg, match) => {
-        var currency = match.input.split(" ")
+    bot.command('borders', (ctx) => {
+        var currency = ctx.message.text.split(" ")
         if (!currency[1]) {
-            bot.sendMessage(msg.chat.id, "Por favor, digite /borders + SÍMBOLO para saber o saldo. Ex:\n /orders BTCBNB")
+            ctx.reply("Por favor, digite /borders + SÍMBOLO para saber o saldo. Ex:\n /borders BTCBNB")
         } else {
-            Exchange.getOrders(currency[1], msg.from.username)
-            .then(resp => bot.sendMessage(msg.chat.id, resp))
+            Exchange.getOrders(currency[1], ctx.from.first_name)
+            .then(resp => ctx.reply(resp))
         } 
+    })
+
+    bot.action(/getBalance (\w+)/, ctx => {
+        var currency = ctx.match[1]
+        Exchange.getBalances(currency, ctx.from.first_name)
+            .then(resp => ctx.reply(resp))
+    })
+    bot.action(/getPrice (\w+)/, ctx => {
+        var currency = ctx.match[1]
+        Exchange.getPrice(currency, ctx.from.first_name)
+            .then(resp => ctx.reply(resp))
     })
 
 module.exports = {
